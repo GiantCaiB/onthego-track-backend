2270c6997a0951f8df9098adc4e269caa0c32ed1
Danny <giantcaib@hotmail.com>
2018-09-12 23:34:20 +1000

    job services debug

--- services/job.js
+++ services/job.js
@@ -1,41 +1,40 @@
-const mongoose = require("mongoose");
-const db = require("../_helpers/db");
+const db = require('../_helpers/db');
 const Job = db.Job;
 
 module.exports = {
-  getAll,
-  getById,
-  create,
-  update,
-  delete: _delete
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
 };
 
 async function getAll() {
-  return await Job.find().select("-hash");
+    return await Job.find().select('-hash');
 }
 
 async function getById(id) {
-  return await Job.findById(id).select("-hash");
+    return await Job.findById(id).select('-hash');
 }
 
 async function create(jobParam) {
-  const job = new Job(jobParam);
-  // save job
-  await job.save();
+    const job = new Job(jobParam);
+    // save job
+    await job.save();
 }
 
 async function update(id, jobParam) {
-  const job = await Job.findById(id);
-  const jobContent = {...jobParam, _id: mongoose.Types.ObjectId(id)}
-  // validate
-  if (!job) throw "Job not found";
+    const job = await Job.findById(id);
 
-  // copy jobParam properties to job
-  Object.assign(job, jobContent);
+    // validate
+    if (!job) throw 'Job not found';
 
-  await job.save();
+    // copy jobParam properties to job
+    Object.assign(job, jobParam);
+
+    await job.save();
 }
 
 async function _delete(id) {
-  await Job.findByIdAndRemove(id);
+    await Job.findByIdAndRemove(id);
 }
