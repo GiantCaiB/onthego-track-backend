627b8e29c657c7ccd62b92b546948a04caf90d7c
Danny <giantcaib@hotmail.com>
2018-09-13 09:05:34 +1000

    wakeUp

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const jwt = require("./_helpers/jwt");
 const errorHandler = require("./_helpers/error_handler");
-const awake = require("./_helpers/awake");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,9 +19,6 @@ app.use("/api/job", require("./routes/jobRoutes"));
 
 // global error handler
 app.use(errorHandler);
-
-// wake heroku app every 30 minutes
-awake.wakeUp(1800000);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
